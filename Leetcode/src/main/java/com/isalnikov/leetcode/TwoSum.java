b970d8a7775a8b3696960b282ef511dee10311db
igor <igor@salnikov>
2021-02-10 17:55:33 +0300

    add

---
+++ Leetcode/src/main/java/com/isalnikov/leetcode/TwoSum.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * https://leetcode.com/problems/two-sum/
+ * https://webdevblog.ru/rassmotreniya-voprosa-two-sum-na-intervju-na-javascript/
+ * https://web.stanford.edu/class/cs9/sample_probs/TwoSum.pdf
+ */
+package com.isalnikov.leetcode;
+
+/**
+ *
+ * @author igor
+ */
+public class TwoSum {
+    public static void main(String[] args) {
+        System.out.println("!!!");
+    }
+}
