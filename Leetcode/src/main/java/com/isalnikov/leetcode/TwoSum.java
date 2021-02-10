b96cdc2a5a3dd723717202bb7dba1477cccfe127
igor <igor@salnikov>
2021-02-10 18:00:50 +0300

    add

--- Leetcode/src/main/java/com/isalnikov/leetcode/TwoSum.java
+++ Leetcode/src/main/java/com/isalnikov/leetcode/TwoSum.java
@@ -1,17 +1,49 @@
 /**
- * 
+ *
  * https://leetcode.com/problems/two-sum/
  * https://webdevblog.ru/rassmotreniya-voprosa-two-sum-na-intervju-na-javascript/
  * https://web.stanford.edu/class/cs9/sample_probs/TwoSum.pdf
  */
 package com.isalnikov.leetcode;
 
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  *
  * @author igor
  */
 public class TwoSum {
-    public static void main(String[] args) {
-        System.out.println("!!!");
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[]{numMap.get(complement), i};
+            } else {
+                numMap.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+
+    public int[] twoSum2(int[] nums, int target) {
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            if (nums[left] + nums[right] == target) {
+                return new int[]{nums[left], nums[right]};
+            } else if (nums[left] + nums[right] < target) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return new int[]{};
     }
 }
+
+
