b3903c0afd9c3c079c161bcc1d717d294763024c
igor <igor@salnikov>
2022-08-31 11:55:38 +0300

    https://leetcode.com/problems/remove-duplicates-from-sorted-array

--- Leetcode/src/main/java/com/isalnikov/leetcode/removeduplicatesfromsortedarray/Solution.java
+++ Leetcode/src/main/java/com/isalnikov/leetcode/removeduplicatesfromsortedarray/Solution.java
@@ -34,3 +34,26 @@ public class Solution {
 //return index;
 //}
 //}
+//
+// С++
+
+//const int ZERO = [](){
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(nullptr);
+//    return 0;
+//}();
+//
+//class Solution {
+//public:
+//    int removeDuplicates(vector<int>& nums) {
+//        if (nums.size() == 0) return 0;
+//        int k = 0;
+//        for (int i = 1; i < nums.size(); i++) {
+//            if (nums[   k ] != nums[i]) {
+//                nums[ ++k ]  = nums[i];
+//            }
+//        }
+//        return k + 1;
+//    }
+//    
+//};
