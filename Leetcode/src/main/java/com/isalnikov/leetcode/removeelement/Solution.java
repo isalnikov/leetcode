a6c8185a26661961c3d1de372aaa22d014f298a3
igor <igor@salnikov>
2022-09-01 15:54:02 +0300

    https://leetcode.com/problems/remove-element/submissions/

--- Leetcode/src/main/java/com/isalnikov/leetcode/removeelement/Solution.java
+++ Leetcode/src/main/java/com/isalnikov/leetcode/removeelement/Solution.java
@@ -34,3 +34,15 @@ public int removeElement(int[] nums, int val) {
         return k;
     }
 }
+//..
+//
+//public int removeElement(int[] nums, int val) {
+//    int i = 0;
+//    for (int j = 0; j < nums.length; j++) {
+//        if (nums[j] != val) {
+//            nums[i] = nums[j];
+//            i++;
+//        }
+//    }
+//    return i;
+//}
