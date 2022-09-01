773d2cbf927c384a5157de138dc685799a36a299
igor <igor@salnikov>
2022-09-01 15:06:44 +0300

    https://leetcode.com/problems/remove-element/submissions/

---
+++ Leetcode/src/main/java/com/isalnikov/leetcode/RemoveElement.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.isalnikov.leetcode;
+
+import com.isalnikov.leetcode.removeelement.Solution;
+
+/**
+ *
+ * @author Igor Salnikov <isalnikov1@gmail.com>
+ */
+public class RemoveElement {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        s.removeElement(new int[]{3,3}, 5);
+    }
+}
---
+++ Leetcode/src/main/java/com/isalnikov/leetcode/removeelement/Solution.java
@@ -0,0 +1,36 @@
+
+package com.isalnikov.leetcode.removeelement;
+
+/**
+ * https://leetcode.com/problems/remove-element/submissions/
+ * @author Igor Salnikov <isalnikov1@gmail.com>
+ */
+public class Solution {
+
+public int removeElement(int[] nums, int val) {
+   if (nums.length == 0) return 0;
+        int k = 0;
+        int i = 0;
+      if (nums.length == 1){
+          if(val == nums[0])  return 0;
+          if(val != nums[0])  return 1;
+      }
+        
+        
+          for (i = 0; i < nums.length; i++){
+              if(val == nums[i]){
+                  k = i;
+                  break;
+              }
+          }
+          if( i == nums.length) return i;
+        
+        for (i = k + 1; i < nums.length; i++) {
+            if (val != nums[i]) {
+                nums[k]  = nums[i];
+                k++;
+            }
+        }
+        return k;
+    }
+}
