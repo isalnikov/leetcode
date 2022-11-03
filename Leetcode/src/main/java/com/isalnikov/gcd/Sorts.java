1fc5be84d8e2c6c9dda0d0bd2dcd50f5ec83e53c
igor <igor@salnikov>
2022-11-03 11:08:57 +0300

    Задача по 8 подбрасываний монеты и 4 герба

---
+++ Leetcode/src/main/java/com/isalnikov/gcd/Sorts.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package com.isalnikov.gcd;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Igor Salnikov <isalnikov1@gmail.com>
+ */
+public class Sorts {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       String [] arr = {"Саркисян","Сальников","Савельев","Сморчков"};
+       Arrays.sort(arr); 
+       System.out.println( Arrays.toString(arr));
+    }
+    
+}
